d663b0cc219088bb987d61d0d8f99c6a7b886583
arafat2201 <marafatbs@gmail.com>
2021-04-02 22:58:08 +0600

    sd-1 mid project
    This is made for sd-1 3rd semester java mid project.
    
    This application does basic banking works.

---
+++ First_Bank.java
@@ -0,0 +1,12 @@
+/**
+ * first_bank
+ */
+public class First_Bank {
+
+    public static void main(String[] args) {
+        Account Arafat = new Account("Arafat", "A0001");
+        Arafat.showMenu();
+    }
+
+   
+}
